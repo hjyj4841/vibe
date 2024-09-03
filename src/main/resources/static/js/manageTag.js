3a387490c73c0c3fbe46282ec9565e5a7629db55
Grainer111 <confidence13w@gmail.com>
2024-09-03 15:25:44 +0900

    fix: TagController, manageTag

--- src/main/resources/static/js/manageTag.js
+++ src/main/resources/static/js/manageTag.js
@@ -4,43 +4,41 @@ document.addEventListener('DOMContentLoaded', function() {
 	// 태그 추가 폼의 유효성 검사
 	const addTagForm = document.getElementById('addTagForm');
 	const addTagInput = document.getElementById('newTag');
-	const addTagError = document.getElementById('addTagError');
 	const playlistCode = document.querySelector('input[name="plCode"]').value;
 
 	addTagForm.addEventListener('submit', function(event) {
-		// 입력된 태그 이름을 가지고 AJAX 요청을 보냄
+		event.preventDefault(); // 폼 제출 기본 동작 방지
+		
 		fetch(`/playlist/checkTag?plCode=${playlistCode}&tagName=${encodeURIComponent(addTagInput.value)}`)
 			.then(response => response.json())
 			.then(data => {
 				if (data.exists) {
-					addTagError.textContent = '이미 존재하는 태그입니다.';
-					event.preventDefault(); // 폼 제출을 막음
+					alert('이미 존재하는 태그입니다.');
 				} else {
-					addTagError.textContent = '';
+					addTagForm.submit(); // 폼 제출
 				}
 			})
 			.catch(error => {
 				console.error('Error:', error);
-				addTagError.textContent = '태그 중복 확인 중 오류 발생.';
-				event.preventDefault(); // 오류 발생 시 폼 제출을 막습니다.
+				alert('태그 중복 확인 중 오류 발생.');
 			});
 	});
 
 	// 태그 삭제 폼의 유효성 검사
 	const deleteTagForm = document.getElementById('deleteTagForm');
-	const deleteTagError = document.getElementById('deleteTagError');
 
 	deleteTagForm.addEventListener('submit', function(event) {
-		// 체크된 태그 수를 확인
-		const checkedTags = document.querySelectorAll('input[name="tagCodes"]:checked');
-		if (checkedTags.length >= MAX_TAGS) {
-			deleteTagError.textContent = '더 이상 추가할 수 없습니다.';
+		const checkedTags = document.querySelectorAll('input[name="tagCodes[]"]:checked');
+		if (checkedTags.length === 0) {
+			alert('삭제할 태그를 선택하세요.');
 			event.preventDefault(); // 폼 제출을 막음
-		} else {
-			deleteTagError.textContent = '';
+		} else if (checkedTags.length > MAX_TAGS) {
+			alert('삭제할 태그 수는 최대 ' + MAX_TAGS + '개입니다.');
+			event.preventDefault(); // 폼 제출을 막음
 		}
 	});
 });
+
 
 $(document).ready(function() {
     let page = 1;
